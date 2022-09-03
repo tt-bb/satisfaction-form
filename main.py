4a44c6f7bbc247dc28825214ccd7569fcc55f008
tt-bb <ttbb_@outlook.com>
2022-09-03 22:13:49 +0200

    Add a basic login/logout system

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask, redirect, render_template, request, session, url_for
 from deta import Deta
+import os
 
 
 app = Flask(__name__, static_url_path='/static')
 app.config['DEBUG'] = True
+# Use to authorize us using session
+app.secret_key = os.getenv('FLASK_KEY')
 
 # CREATE DB
 deta = Deta()
 form_db = deta.Base('form_submissions')
-# KEY NAME = nm8cui
-# PROJECT KEY = a07a51xw_KKtv7CTeqx36pq1GohkzuQSyhhRWy9ov
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -29,36 +30,42 @@ def index():
         return render_template('submit.html')
 
 
+# ASK ADMIN ACCESS
 @app.route('/forms')
 def forms():
-    # all_forms=[
-    #     {
-    #         'user_name': 'TTBB',
-    #         'dealer_name': 'Benjamin',
-    #         'rating': '10',
-    #         'comments': 'Comment'
-    #     }
-    # ]
-    forms_object = form_db.fetch()
-    all_forms = forms_object.items
-    return render_template('forms.html', forms=all_forms)
+    if not session.get('logged_in'):
+        return render_template('login.html', message=None)
+    else:
+        # all_forms=[
+        #     {
+        #         'user_name': 'TTBB',
+        #         'dealer_name': 'Benjamin',
+        #         'rating': '10',
+        #         'comments': 'Comment'
+        #     }
+        # ]
+        forms_object = form_db.fetch()
+        all_forms = forms_object.items
+        return render_template('forms.html', forms=all_forms)
 
 
-# @app.route('/login', methods=['POST'])
-# def login():
-#     if request.method == 'POST':
-#         if request.form['username'] == 'ttbb' and request.form['password'] == 'p@$s|v/ord':
-#             session['logged_in'] = True
-#             return redirect(url_for('forms'))
-#         else:
-#             return render_template('login.html')
-#     else:
-#         return redirect(url_for('index'))
-#
-# @app.route("/logout")
-# def logout():
-#     session['logged_in'] = False
-#     return redirect(url_for('index'))
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] == 'ttbb' and request.form['password'] == 'p@$s|v/ord':
+            session['logged_in'] = True
+            return redirect(url_for('forms'))
+        else:
+            message = 'Wrong username or password...'
+            return render_template('login.html', message=message)
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
