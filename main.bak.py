326cfd202d89a345a8c411d832c2f3b9d8b2f103
tt-bb <ttbb_@outlook.com>
2022-09-04 11:16:31 +0200

    Rolling back

--- main.bak.py
+++ main.bak.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request, session, url_for
 from deta import Deta
+import bcrypt
 import os
 
 
@@ -8,9 +9,19 @@ app.config['DEBUG'] = False
 # Use to authorize us using session
 app.secret_key = os.getenv('FLASK_KEY')
 
-# CREATE DB
+# CREATE DBS
 deta = Deta()
 form_db = deta.Base('form_submissions')
+admins = deta.Base('admins')
+
+# CREATE ADMINS
+salt = os.getenv('SALT')
+salt = salt.encode("utf-8")
+admin_name = 'Tiffanie'
+password = 'Agdl98jfsmpAq555'.encode('utf-8')
+hashed = bcrypt.hashpw(password, salt)
+hashed = str(hashed)
+admins.put({'admin_name':admin_name, 'password': hashed})
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -52,7 +63,14 @@ def forms():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        if request.form['username'] == 'ttbb' and request.form['password'] == 'p@$s|v/ord':
+        admins_object = admins.fetch()
+        all_admins = admins_object.items
+        if request.form['username'] in all_admins:
+            password = request.form['password']
+            password_bytes = password.encode("utf-8")
+
+
+            bcrypt.checkpw(password_bytes, )
             session['logged_in'] = True
             return redirect(url_for('forms'))
         else:
